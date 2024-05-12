37d308fce2b0b91200f253646e877add37c291b0
MatviiTorop <ipz224_tmd@student.ztu.edu.ua>
2024-05-12 22:30:58 +0300

    System of board classes was simplified by adding realization for printing board in base class

--- TicTacToeClassLibrary/Board3.cs
+++ TicTacToeClassLibrary/Board3.cs
@@ -15,17 +15,6 @@ namespace TicTacToeClassLibrary
                 { "4", "5", "6" }, 
                 { "7", "8", "9" } };
         }
-        public override void Print()
-        {
-            for(int i = 0; i < this.Lattice.GetLength(0); i++)
-            {
-                for(int j = 0; j < this.Lattice.GetLength(1); j++)
-                {
-                    Console.Write(this.Lattice[i, j]);
-                    Console.Write(" | ");
-                }
-                Console.WriteLine("\n--- --- ---");
-            }
-        }
+        
     }
 }
